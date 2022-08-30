19241269d1e7fa05ed67127c8c431d60f3bff64f
Mish880 <Pereraittec6@gmail.com>
2022-08-30 12:42:00 +0530

    UpdateRegistration

--- src/page/UserRegistration/UserRegistration.jsx
+++ src/page/UserRegistration/UserRegistration.jsx
@@ -1,64 +1,30 @@
-import { Table, TableContainer } from '@mui/material'
 import React from 'react'
 import "./UserRegistration.css"
+import  Typography  from '@mui/material/Typography';
+import  TextField  from '@mui/material/TextField';
+import  Button from '@mui/material/Button';
+import  Table from '@mui/material/Table';
+import  TableBody from '@mui/material/TableBody';
+import  TableCell  from '@mui/material/TableCell';
+import  TableContainer  from '@mui/material/TableContainer';
+import  TableHead  from '@mui/material/TableHead';
 
 const UserRegistrationform = ()  =>{
   return (
-   
-         <div className='main_form'>
         
-         <form>
-           <div className='container'>
-               <h1>User Registration</h1>
-               <p>Please fill this form to create an account</p>
-            
-                
-               <label for="firstname"><b>Firstname</b></label>
-               <input type="text" placeholder="Enter Firstname" name="" id="firstname" required></input>
+      <div>
 
-               <label for="lastname"><b>Lastname</b></label>
-               <input type="text" placeholder="Enter Lastname" name="" id="lastname" required></input>
+          <Typography variant="h5" gutterBottom style={{fontSize:'38px', fontFamily:'comfortea, sans-serif'}} component="div">
+           UserRegistration
+         </Typography>
 
-               <label for="email"><b>Email</b></label>
-               <input type="text" placeholder="Enter Email" name="" id="email" required></input>
-              
-               <label for="contact"><b>Contact</b></label>
-               <input type="text" placeholder="Enter Contact" name="" id="contact" required></input>
-
-               <p>By creating an account you agree to our <a href='#'>Terms & Privacy</a>.</p>
-
-               <button type='submit' className='registerbtn'>Registration</button>
-
-               <div className='signin'>
-                 <p>Already have an account? <a href="#">Sign in</a>.</p>
-               </div>
-
-           </div>
-         </form>
-      
-         <div>
-             <h2> User Registration Table</h2>
-    
          
-          <table>
-              <tr>
-                 <th>First Name</th>
-                 <th>Last Name</th>
-                 <th>Email</th>
-                 <th>Contact</th>
-              </tr>
-             <tr>
-             <td> dcdcdc</td>
-             <td> dcdcdc</td>
-             <td> dcdcdc</td>
-             <td> dcdcdc</td>
-             </tr>
-              
-           </table>
-          </div>
-        
+      
 
-         </div>
+
+
+      </div>
+         
 
 
       

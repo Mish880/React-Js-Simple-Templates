54abb31c8c3b093c7f1cc0f7908dabbd752c3410
Mish880 <Pereraittec6@gmail.com>
2022-08-30 12:03:28 +0530

    Update Cart

--- src/page/CartManage/CartManage.jsx
+++ src/page/CartManage/CartManage.jsx
@@ -15,16 +15,36 @@ const CartManage = () => {
        </Typography>
      
         <div>
-           <Autocomplete>
-                
-           
-           
-           </Autocomplete>  
-        </div> 
+        <Autocomplete
+          disablePortal
+         id="combo-box-demo"
+         /*options={top100Films}*/
+         sx={{ width: 300 }}
+         renderInput={(params) => <TextField {...params} label="Vegatables" />}
+        />
 
+        <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        /*options={top100Films}*/
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="foods" />}
+        />
+    </div> 
+
+       <div>
+        <TextField id="standard-basic" label="Date" variant="standard" className='txt1'/>
+        <TextField id="standard-basic" label="qty" variant="standard" className='txt2'/> 
+       </div> 
+
+       <div>
+
+       </div>
+       <Button variant="contained" style={{width:'14vh', left:'140vh' , top:'229px'}} className='buttonone' color='primary'>Save</Button>
+       <Button variant="contained" style={{width:'14vh', top:'229px'}} className='buttontwo'>Cancel</Button>
         </div>
     </div>
   );
 }
 
-export default CartManage
+export default CartManage;

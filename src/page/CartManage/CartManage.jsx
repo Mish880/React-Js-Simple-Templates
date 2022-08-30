ec15e70f6ecf2f1d68007082c2ce7e99e45081ab
Mish880 <Pereraittec6@gmail.com>
2022-08-30 11:54:15 +0530

    Update Cart

--- src/page/CartManage/CartManage.jsx
+++ src/page/CartManage/CartManage.jsx
@@ -1,11 +1,30 @@
-
 import React from 'react'
 import './CartManage.css'
+import Typography  from '@mui/material/Typography';
+import Autocomplete  from '@mui/material/Autocomplete';
+import TextField  from '@mui/material/TextField';
+import Button  from '@mui/material/Button'; 
 
 const CartManage = () => {
   return (
-    <div>CartManage</div>
-  )
+    <div>
+        <div>
+        <Typography variant="h5" gutterBottom style={{fontSize: '38px', fontFamily: 'comfortaa, sans-serif'}}
+         component="div">
+           Cart Manage
+       </Typography>
+     
+        <div>
+           <Autocomplete>
+                
+           
+           
+           </Autocomplete>  
+        </div> 
+
+        </div>
+    </div>
+  );
 }
 
 export default CartManage
